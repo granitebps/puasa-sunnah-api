401bd1517a9592440b9e362141c68258a3d624d3
Granite Bagas <granitebagas28@gmail.com>
2022-04-16 15:06:22 +0700

    :construction: PSN-4 Setup fiber. Add root url and not found handler

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
 func main() {
-	//
+	app := fiber.New()
+
+	app.Use(logger.New())
+	app.Use(limiter.New(limiter.Config{
+		Max: 60,
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Puasa Sunnah API")
+	})
+
+	app.Get("*", func(c *fiber.Ctx) error {
+		return c.SendStatus(404)
+	})
+
+	app.Listen(":3000")
 }
