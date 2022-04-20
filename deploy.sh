c2ff58b9f6f21c19c1607ec505eeebe73f8f4adb
Granite Bagas <granitebagas28@gmail.com>
2022-04-20 21:17:27 +0700

    :rocket: Fix deployment script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Update codebase
+git fetch origin main
+git reset --hard origin/main
+
+# Generate docs
+make doc
+
+# Build the application
+make build
+
+# Restart the service
+sudo service psn restart
+
+echo "Application deployed!"
