f290570dca5301fe8bdda1743ccb7d9119f80bdb
Granite Bagas <granitebagas28@gmail.com>
2023-04-20 21:50:09 +0700

    :construction: Update recover handler

--- routes/index.go
+++ routes/index.go
@@ -1,21 +1,43 @@
 package routes
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/swagger"
-	"github.com/granitebps/puasa-sunnah-api/docs"
 	"github.com/granitebps/puasa-sunnah-api/helpers"
-	"github.com/spf13/viper"
+	"github.com/granitebps/puasa-sunnah-api/middleware"
 )
 
-func InitRoutes(app *fiber.App) *fiber.App {
+func InitRoutes() *fiber.App {
+	app := fiber.New(fiber.Config{
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			// Status code defaults to 500
+			code := fiber.StatusInternalServerError
+			msg := "Internal Server Error"
+
+			// Retrieve the custom status code if it's a *fiber.Error
+			var e *fiber.Error
+			if errors.As(err, &e) {
+				code = e.Code
+				msg = e.Message
+			}
+
+			return ctx.Status(code).JSON(helpers.FailedAPIResponse(
+				msg,
+				code,
+			))
+		},
+	})
+
+	// Initialize Middlewares
+	middleware.InitMiddleware(app)
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Puasa Sunnah API")
 	})
 
-	docs.SwaggerInfo.Host = viper.GetString("SWAGGER_HOST")
 	app.Get("/swagger/*", swagger.HandlerDefault) // default
 
 	api := app.Group("api/v1")
