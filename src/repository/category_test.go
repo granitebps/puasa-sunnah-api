ac1e6b3b3de053fa2fed0b1ff50f6c6987f581a2
Granite Bagas <granitebagas28@gmail.com>
2024-03-23 23:18:26 +0700

    test: PSN-52 Add category test

--- src/repository/category_test.go
+++ src/repository/category_test.go
@@ -12,71 +12,69 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSourceGetAll(t *testing.T) {
-	query := regexp.QuoteMeta("SELECT * FROM `sources`")
-	t.Run("should return all sources", func(t *testing.T) {
-		rows := sqlmock.NewRows([]string{"id", "url"}).
-			AddRow(1, "https://example.com")
+func TestCategoryGetAll(t *testing.T) {
+	query := regexp.QuoteMeta("SELECT * FROM `categories`")
+	t.Run("should return all categories", func(t *testing.T) {
+		rows := sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "Category Name")
 		mock.ExpectQuery(query).WillReturnRows(rows)
 
-		repo := repository.NewSourceRepository(c)
+		repo := repository.NewCategoryRepository(c)
 		sources, err := repo.GetAll(context.Background())
 		assert.NoError(t, err)
 		assert.Len(t, sources, 1)
 		assert.Equal(t, sources[0].ID, uint(1))
-		assert.Equal(t, sources[0].Url, "https://example.com")
+		assert.Equal(t, sources[0].Name, "Category Name")
 	})
 	t.Run("should handle failed query", func(t *testing.T) {
-		sqlmock.NewRows([]string{"id", "url"})
+		sqlmock.NewRows([]string{"id", "name"})
 		errMock := errors.New("failed query")
 		mock.ExpectQuery(query).WillReturnError(errMock)
 
-		repo := repository.NewSourceRepository(c)
+		repo := repository.NewCategoryRepository(c)
 		sources, err := repo.GetAll(context.Background())
 		assert.ErrorIs(t, err, errMock)
 		assert.Len(t, sources, 0)
 	})
 }
 
-func TestSourceGetByID(t *testing.T) {
-	query := regexp.QuoteMeta("SELECT * FROM `sources`")
-	t.Run("should return source", func(t *testing.T) {
-		rows := sqlmock.NewRows([]string{"id", "url"}).
-			AddRow(1, "https://example.com")
+func TestCategoryGetByID(t *testing.T) {
+	query := regexp.QuoteMeta("SELECT * FROM `categories`")
+	t.Run("should return category", func(t *testing.T) {
+		rows := sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "Category Name")
 		mock.ExpectQuery(query).WillReturnRows(rows)
 
-		repo := repository.NewSourceRepository(c)
+		repo := repository.NewCategoryRepository(c)
 		source, err := repo.GetByID(context.Background(), 1)
 		assert.NoError(t, err)
 		assert.Equal(t, source.ID, uint(1))
-		assert.Equal(t, source.Url, "https://example.com")
+		assert.Equal(t, source.Name, "Category Name")
 	})
 	t.Run("should handle failed query", func(t *testing.T) {
-		sqlmock.NewRows([]string{"id", "url"})
+		sqlmock.NewRows([]string{"id", "name"})
 		errMock := errors.New("failed query")
 		mock.ExpectQuery(`SELECT`).WillReturnError(errMock)
 
-		repo := repository.NewSourceRepository(c)
-		source, err := repo.GetByID(context.Background(), 1)
+		repo := repository.NewCategoryRepository(c)
+		cat, err := repo.GetByID(context.Background(), 1)
 		assert.ErrorIs(t, err, errMock)
-		assert.Equal(t, source.ID, uint(0))
+		assert.Equal(t, cat.ID, uint(0))
 	})
 }
 
-func TestSourceCreate(t *testing.T) {
-	query := regexp.QuoteMeta("INSERT INTO `sources`")
-	sourceData := model.Source{
-		Url: "https://example.com",
-	}
-	t.Run("should create source", func(t *testing.T) {
+func TestCategoryCreate(t *testing.T) {
+	query := regexp.QuoteMeta("INSERT INTO `categories`")
+	catData := model.Category{Name: "Category Name"}
+	t.Run("should create category", func(t *testing.T) {
 		mock.ExpectBegin()
 		mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
 		mock.ExpectCommit()
 
-		repo := repository.NewSourceRepository(c)
-		err := repo.Create(context.Background(), &sourceData)
+		repo := repository.NewCategoryRepository(c)
+		err := repo.Create(context.Background(), &catData)
 		assert.NoError(t, err)
-		assert.Equal(t, sourceData.ID, uint(1))
+		assert.Equal(t, catData.ID, uint(1))
 	})
 	t.Run("should handle failed query", func(t *testing.T) {
 		errMock := errors.New("failed query")
@@ -84,25 +82,25 @@ func TestSourceCreate(t *testing.T) {
 		mock.ExpectExec(query).WillReturnError(errMock)
 		mock.ExpectRollback()
 
-		repo := repository.NewSourceRepository(c)
-		err := repo.Create(context.Background(), &sourceData)
+		repo := repository.NewCategoryRepository(c)
+		err := repo.Create(context.Background(), &catData)
 		assert.ErrorIs(t, err, errMock)
 	})
 }
 
-func TestSourceUpdate(t *testing.T) {
-	query := regexp.QuoteMeta("UPDATE `sources`")
-	sourceData := model.Source{
-		ID:  1,
-		Url: "https://example.com",
+func TestCategoryUpdate(t *testing.T) {
+	query := regexp.QuoteMeta("UPDATE `categories`")
+	catData := model.Category{
+		ID:   1,
+		Name: "Category Name",
 	}
-	t.Run("should update source", func(t *testing.T) {
+	t.Run("should update category", func(t *testing.T) {
 		mock.ExpectBegin()
 		mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
 		mock.ExpectCommit()
 
-		repo := repository.NewSourceRepository(c)
-		err := repo.Update(context.Background(), &sourceData)
+		repo := repository.NewCategoryRepository(c)
+		err := repo.Update(context.Background(), &catData)
 		assert.NoError(t, err)
 	})
 	t.Run("should handle failed query", func(t *testing.T) {
@@ -111,8 +109,8 @@ func TestSourceUpdate(t *testing.T) {
 		mock.ExpectExec(query).WillReturnError(errMock)
 		mock.ExpectRollback()
 
-		repo := repository.NewSourceRepository(c)
-		err := repo.Update(context.Background(), &sourceData)
+		repo := repository.NewCategoryRepository(c)
+		err := repo.Update(context.Background(), &catData)
 		assert.ErrorIs(t, err, errMock)
 	})
 }
