74be2d2943206a23d0fd0f2e78244d066faca9a3
Granite Bagas <granitebagas28@gmail.com>
2024-03-23 23:51:40 +0700

    test: PSN-52 Add types service test

---
+++ src/service/types_test.go
@@ -0,0 +1,42 @@
+package service_test
+
+import (
+	"context"
+	"errors"
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/granitebps/puasa-sunnah-api/src/repository"
+	"github.com/granitebps/puasa-sunnah-api/src/service"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTypesGetAll(t *testing.T) {
+	query := regexp.QuoteMeta("SELECT * FROM `types`")
+	t.Run("should return all types", func(t *testing.T) {
+		rows := sqlmock.NewRows([]string{"id", "name", "description"}).
+			AddRow(1, "John Doe", "Test")
+		mock.ExpectQuery(query).WillReturnRows(rows)
+
+		repo := repository.NewTypesRepository(c)
+		serv := service.NewTypesService(repo)
+		types, err := serv.GetAll(context.Background())
+		assert.NoError(t, err)
+		assert.Len(t, types, 1)
+		assert.Equal(t, types[0].ID, uint(1))
+		assert.Equal(t, types[0].Name, "John Doe")
+		assert.Equal(t, types[0].Description, "Test")
+	})
+	t.Run("should handle failed query", func(t *testing.T) {
+		sqlmock.NewRows([]string{"id", "name", "description"})
+		errMock := errors.New("failed query")
+		mock.ExpectQuery(query).WillReturnError(errMock)
+
+		repo := repository.NewTypesRepository(c)
+		serv := service.NewTypesService(repo)
+		types, err := serv.GetAll(context.Background())
+		assert.ErrorIs(t, err, errMock)
+		assert.Len(t, types, 0)
+	})
+}
