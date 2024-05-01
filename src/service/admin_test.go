aa7ef2de24d6918ffafdb69c841cfd1f3f2f211b
Granite Bagas <granitebagas28@gmail.com>
2024-05-01 22:05:58 +0700

    test: PSN-52 Add create & update fasting test

--- src/service/admin_test.go
+++ src/service/admin_test.go
@@ -5,8 +5,11 @@ import (
 	"errors"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/ansel1/merry/v2"
+	"github.com/gofiber/fiber/v2"
 	"github.com/granitebps/puasa-sunnah-api/src/requests"
 	"github.com/stretchr/testify/assert"
 )
@@ -214,6 +217,243 @@ func TestAdminUpdateType(t *testing.T) {
 
 		res, err := adminService.UpdateType(context.Background(), 1, &req)
 		assert.ErrorIs(t, err, errMock)
+		assert.Equal(t, res.ID, uint(0))
+	})
+}
+
+func TestAdminCreateFasting(t *testing.T) {
+	query := regexp.QuoteMeta("INSERT INTO `fastings`")
+	queryFasting := regexp.QuoteMeta("SELECT * FROM `fastings`")
+	queryCat := regexp.QuoteMeta("SELECT * FROM `categories`")
+	queryType := regexp.QuoteMeta("SELECT * FROM `types`")
+	req := requests.FastingCreateUpdateRequest{
+		CategoryID: 1,
+		TypeID:     1,
+		Date:       "2021-01-01",
+		Year:       2021,
+		Month:      1,
+		Day:        1,
+	}
+	t.Run("should create fasting", func(t *testing.T) {
+		now := time.Now()
+		rowsFasting := sqlmock.NewRows([]string{"id", "category_id", "type_id", "date", "year", "month", "day"}).
+			AddRow(1, 1, 1, now, "2021", "01", "01")
+		rowsCat := sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "Category Name")
+		rowsType := sqlmock.NewRows([]string{"id", "name", "description"}).
+			AddRow(1, "John Doe", "Test")
+
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnError(errors.New("not found"))
+		mock.ExpectBegin()
+		mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+		mock.ExpectQuery(queryFasting).WillReturnRows(rowsFasting)
+		mock.ExpectQuery(queryCat).WillReturnRows(rowsCat)
+		mock.ExpectQuery(queryType).WillReturnRows(rowsType)
+
+		res, err := adminService.CreateFasting(context.Background(), &req)
+		assert.NoError(t, err)
+		assert.Equal(t, res.ID, uint(1))
+	})
+	t.Run("should handle failed query: create", func(t *testing.T) {
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnError(errors.New("not found"))
+		mock.ExpectBegin()
+		mock.ExpectExec(query).WillReturnError(errors.New("failed query"))
+		mock.ExpectRollback()
+
+		res, err := adminService.CreateFasting(context.Background(), &req)
+		assert.Equal(t, res.ID, uint(0))
+		assert.Equal(t, err.Error(), "failed query")
+	})
+	t.Run("should handle failed query: get by ID", func(t *testing.T) {
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnError(errors.New("not found"))
+		mock.ExpectBegin()
+		mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+		mock.ExpectQuery(queryFasting).WillReturnError(errors.New("not found"))
+
+		res, err := adminService.CreateFasting(context.Background(), &req)
+		assert.Equal(t, err.Error(), "not found")
+		assert.Equal(t, res.ID, uint(0))
+	})
+	t.Run("should handle failed query: fasting exists", func(t *testing.T) {
+		now := time.Now()
+		rowsFasting := sqlmock.NewRows([]string{"id", "category_id", "type_id", "date", "year", "month", "day"}).
+			AddRow(1, 1, 1, now, "2021", "01", "01")
+		rowsCat := sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "Category Name")
+		rowsType := sqlmock.NewRows([]string{"id", "name", "description"}).
+			AddRow(1, "John Doe", "Test")
+
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnRows(rowsFasting)
+		mock.ExpectQuery(queryCat).WillReturnRows(rowsCat)
+		mock.ExpectQuery(queryType).WillReturnRows(rowsType)
+
+		res, err := adminService.CreateFasting(context.Background(), &req)
+		assert.Equal(t, err.Error(), "Fasting already exists")
+		assert.Equal(t, merry.HTTPCode(err), fiber.StatusBadRequest)
+		assert.Equal(t, res.ID, uint(0))
+	})
+	t.Run("should handle failed query: date format not valid", func(t *testing.T) {
+		req.Date = "123456789"
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnError(errors.New("not found"))
+
+		res, err := adminService.CreateFasting(context.Background(), &req)
+		assert.Equal(t, merry.UserMessage(err), "Invalid date format")
+		assert.Equal(t, merry.HTTPCode(err), fiber.StatusBadRequest)
+		assert.Equal(t, res.ID, uint(0))
+	})
+	t.Run("should handle failed query: date not valid", func(t *testing.T) {
+		req.Date = "2021-01-01"
+		req.Year = 1234
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnError(errors.New("not found"))
+
+		res, err := adminService.CreateFasting(context.Background(), &req)
+		assert.Equal(t, err.Error(), "Invalid date")
+		assert.Equal(t, merry.HTTPCode(err), fiber.StatusBadRequest)
+		assert.Equal(t, res.ID, uint(0))
+	})
+}
+
+func TestAdminUpdateFasting(t *testing.T) {
+	query := regexp.QuoteMeta("UPDATE `fastings`")
+	queryFasting := regexp.QuoteMeta("SELECT * FROM `fastings`")
+	queryCat := regexp.QuoteMeta("SELECT * FROM `categories`")
+	queryType := regexp.QuoteMeta("SELECT * FROM `types`")
+	req := requests.FastingCreateUpdateRequest{
+		CategoryID: 1,
+		TypeID:     1,
+		Date:       "2021-01-01",
+		Year:       2021,
+		Month:      1,
+		Day:        1,
+	}
+	t.Run("should update fasting", func(t *testing.T) {
+		now := time.Now()
+		rowsFasting := sqlmock.NewRows([]string{"id", "category_id", "type_id", "date", "year", "month", "day"}).
+			AddRow(1, 1, 1, now, "2021", "01", "01").
+			AddRow(2, 1, 1, now, "2021", "01", "01")
+		rowsCat := sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "Category Name")
+		rowsType := sqlmock.NewRows([]string{"id", "name", "description"}).
+			AddRow(1, "John Doe", "Test")
+
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnRows(rowsFasting)
+		mock.ExpectQuery(queryCat).WillReturnRows(rowsCat)
+		mock.ExpectQuery(queryType).WillReturnRows(rowsType)
+		mock.ExpectQuery(queryFasting).WillReturnError(errors.New("not found"))
+		mock.ExpectBegin()
+		mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(0, 1))
+		mock.ExpectCommit()
+		mock.ExpectQuery(queryFasting).WillReturnRows(rowsFasting)
+		mock.ExpectQuery(queryCat).WillReturnRows(rowsCat)
+		mock.ExpectQuery(queryType).WillReturnRows(rowsType)
+
+		res, err := adminService.UpdateFasting(context.Background(), &req, 2)
+		assert.NoError(t, err)
+		assert.Equal(t, res.ID, uint(2))
+	})
+	t.Run("should handle failed query: update", func(t *testing.T) {
+		now := time.Now()
+		rowsFasting := sqlmock.NewRows([]string{"id", "category_id", "type_id", "date", "year", "month", "day"}).
+			AddRow(1, 1, 1, now, "2021", "01", "01")
+		rowsCat := sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "Category Name")
+		rowsType := sqlmock.NewRows([]string{"id", "name", "description"}).
+			AddRow(1, "John Doe", "Test")
+
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnRows(rowsFasting)
+		mock.ExpectQuery(queryCat).WillReturnRows(rowsCat)
+		mock.ExpectQuery(queryType).WillReturnRows(rowsType)
+		mock.ExpectBegin()
+		mock.ExpectExec(query).WillReturnError(errors.New("failed query"))
+		mock.ExpectRollback()
+
+		res, err := adminService.UpdateFasting(context.Background(), &req, 1)
+		assert.Equal(t, res.ID, uint(0))
+		assert.Equal(t, err.Error(), "failed query")
+	})
+	t.Run("should handle failed query: first get by ID", func(t *testing.T) {
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnError(errors.New("not found"))
+
+		res, err := adminService.UpdateFasting(context.Background(), &req, 1)
+		assert.Equal(t, err.Error(), "not found")
+		assert.Equal(t, res.ID, uint(0))
+	})
+	t.Run("should handle failed query: second get by ID", func(t *testing.T) {
+		now := time.Now()
+		rowsFasting := sqlmock.NewRows([]string{"id", "category_id", "type_id", "date", "year", "month", "day"}).
+			AddRow(1, 1, 1, now, "2021", "01", "01")
+		rowsCat := sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "Category Name")
+		rowsType := sqlmock.NewRows([]string{"id", "name", "description"}).
+			AddRow(1, "John Doe", "Test")
+
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnRows(rowsFasting)
+		mock.ExpectQuery(queryCat).WillReturnRows(rowsCat)
+		mock.ExpectQuery(queryType).WillReturnRows(rowsType)
+		mock.ExpectBegin()
+		mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+		mock.ExpectQuery(queryFasting).WillReturnError(errors.New("not found"))
+
+		res, err := adminService.UpdateFasting(context.Background(), &req, 1)
+		assert.Equal(t, err.Error(), "not found")
+		assert.Equal(t, res.ID, uint(0))
+	})
+	t.Run("should handle failed query: fasting exists", func(t *testing.T) {
+		now := time.Now()
+		tom := time.Now().Add(time.Hour * 24)
+		rowsFasting := sqlmock.NewRows([]string{"id", "category_id", "type_id", "date", "year", "month", "day"}).
+			AddRow(1, 1, 1, now, "2021", "01", "01").
+			AddRow(2, 1, 1, tom, "2021", "01", "01")
+		rowsCat := sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "Category Name")
+		rowsType := sqlmock.NewRows([]string{"id", "name", "description"}).
+			AddRow(1, "John Doe", "Test")
+
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnRows(rowsFasting)
+		mock.ExpectQuery(queryCat).WillReturnRows(rowsCat)
+		mock.ExpectQuery(queryType).WillReturnRows(rowsType)
+		mock.ExpectQuery(queryFasting).WillReturnRows(rowsFasting)
+		mock.ExpectQuery(queryCat).WillReturnRows(rowsCat)
+		mock.ExpectQuery(queryType).WillReturnRows(rowsType)
+
+		req.Date = "2021-01-02"
+		res, err := adminService.UpdateFasting(context.Background(), &req, 1)
+		assert.Equal(t, err.Error(), "Fasting already exists")
+		assert.Equal(t, merry.HTTPCode(err), fiber.StatusBadRequest)
+		assert.Equal(t, res.ID, uint(0))
+	})
+	t.Run("should handle failed query: date format not valid", func(t *testing.T) {
+		req.Date = "123456789"
+		now := time.Now()
+		rowsFasting := sqlmock.NewRows([]string{"id", "category_id", "type_id", "date", "year", "month", "day"}).
+			AddRow(1, 1, 1, now, "2021", "01", "01")
+		rowsCat := sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "Category Name")
+		rowsType := sqlmock.NewRows([]string{"id", "name", "description"}).
+			AddRow(1, "John Doe", "Test")
+
+		mock.MatchExpectationsInOrder(true)
+		mock.ExpectQuery(queryFasting).WillReturnRows(rowsFasting)
+		mock.ExpectQuery(queryCat).WillReturnRows(rowsCat)
+		mock.ExpectQuery(queryType).WillReturnRows(rowsType)
+
+		res, err := adminService.UpdateFasting(context.Background(), &req, 1)
+		assert.Equal(t, merry.UserMessage(err), "Invalid date format")
+		assert.Equal(t, merry.HTTPCode(err), fiber.StatusBadRequest)
 		assert.Equal(t, res.ID, uint(0))
 	})
 }
